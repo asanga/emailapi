80b72b838b8f325b4f0db8fb5544dd1b5a621215
Asanga Bandara Wijekoon <asangawije@gmail.com>
2023-05-16 23:09:20 +1200

    Initial commit
    Included web-starter, actuator and lombok dependencies for the initial commit

---
+++ src/test/java/com/aviation/emailapi/EmailapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviation.emailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
